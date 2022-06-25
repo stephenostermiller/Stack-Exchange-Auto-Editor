e5ec638d16950f0f625bffc6b4d18b0666bce4d6
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2022-06-24 20:52:36 -0400

    Bump version

--- editor.meta.js
+++ editor.meta.js
@@ -6,7 +6,7 @@
 // @developer sathyabhat
 // @contributor Unihedron
 // @license MIT
-// @version 1.0.7
+// @version 1.0.8
 // @namespace https://github.com/stephenostermiller/Stack-Exchange-Auto-Editor
 // @updateURL https://github.com/stephenostermiller/Stack-Exchange-Auto-Editor/raw/master/editor.meta.js
 // @downloadURL https://github.com/stephenostermiller/Stack-Exchange-Auto-Editor/raw/master/editor.user.js
